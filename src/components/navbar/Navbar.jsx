7180f4cd03289d082fef5f8a6372e67079254b83
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2025-01-02 15:47:07 +0530

    apply animation to Name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Navbar.css'
+import {motion} from 'framer-motion'
 
 function Navbar() {
   return (
     <div className='navbar'>
         {/* Sidebar */}
         <div className="wrapper">
-            <span>Saneepa Pharmacy</span>
+            <motion.span
+                initial={{opacity: 0, scale: 0}}
+                animate={{opacity: 1, scale: 1}}
+                transition={{duration: 1.5}}
+            >Saneepa Pharmacy</motion.span>
             <div className='social'>
                 <a href='#'><img src='src/assets/facebook.png' alt=''/></a>
                 <a href='#'><img src='src/assets/facebook.png' alt=''/></a>
